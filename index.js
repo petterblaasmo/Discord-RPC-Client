92cc1ca282062a1ddf102e99eb3b41396be1a6c2
petterblaasmo <petterblaasmo@gmail.com>
2022-03-19 16:53:36 +0100

    19.03.22 - 16:53

--- index.js
+++ index.js
@@ -6,100 +6,108 @@ const clientId = "916233710364409858",
   fs = require("fs"),
   chalk = require("chalk");
 
-let amount = 0;
+const run = () => {
+  let amount = 0;
 
-const updateActivity = () => {
-  const checkStat = util.promisify(fs.stat),
-    checkDir = util.promisify(fs.readdir);
+  const updateActivity = () => {
+    const checkStat = util.promisify(fs.stat),
+      checkDir = util.promisify(fs.readdir);
 
-  const getFiles = async (dir) => {
-    const files = await checkDir(dir);
-    return Promise.all(
-      files
-        .filter(
-          (f) =>
-            f !== "node_modules" &&
-            f !== ".git" &&
-            f !== "gokz-edit" &&
-            f !== "include" &&
-            !f.includes(".mp4") &&
-            !f.includes(".png") &&
-            !f.includes(".jpg") &&
-            !f.includes(".gif") &&
-            !f.includes("package-lock.json") &&
-            !f.includes("spcomp") &&
-            !f.includes("kztimer") &&
-            !f.includes("compile")
-        )
-        .map((f) => path.join(dir, f))
-        .map(async (f) => {
-          const stats = await checkStat(f);
-          return stats.isDirectory() ? getFiles(f) : f;
-        })
-    );
-  };
-
-  const flattenArray = (array) => {
-    return array.reduce(
-      (flat, toFlatten) =>
-        flat.concat(
-          Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten
-        ),
-      []
-    );
-  };
-
-  getFiles("C:/github")
-    .then(flattenArray)
-    .then((files) => {
-      files.map((f) =>
-        fs.readFile(f, "utf8", (err, data) => {
-          amount = amount + data.split("\n").length;
-        })
+    const getFiles = async (dir) => {
+      const files = await checkDir(dir);
+      return Promise.all(
+        files
+          .filter(
+            (f) =>
+              f !== "node_modules" &&
+              f !== ".git" &&
+              f !== "gokz-edit" &&
+              f !== "include" &&
+              !f.includes(".mp4") &&
+              !f.includes(".png") &&
+              !f.includes(".jpg") &&
+              !f.includes(".gif") &&
+              !f.includes("package-lock.json") &&
+              !f.includes("spcomp") &&
+              !f.includes("kztimer") &&
+              !f.includes("compile")
+          )
+          .map((f) => path.join(dir, f))
+          .map(async (f) => {
+            const stats = await checkStat(f);
+            return stats.isDirectory() ? getFiles(f) : f;
+          })
       );
-    });
+    };
 
-  setTimeout(() => {
-    client.setActivity({
-      details: "Developing",
-      state: `${amount.toLocaleString(undefined, {
-        minimumFractionDigits: 0,
-      })} total lines of code`,
-      largeImageKey: `https://celeritycs.com/images/background.png`,
-      largeImageText: `www.celeritycs.com`,
-      smallImageKey: `https://celeritycs.com/images/white.png`,
-      smallImageText: `Celerity`,
-      instance: true,
-      buttons: [
-        {
-          label: "Website",
-          url: "https://celeritycs.com",
-        },
-      ],
-    });
-  }, 1000);
-};
+    const flattenArray = (array) => {
+      return array.reduce(
+        (flat, toFlatten) =>
+          flat.concat(
+            Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten
+          ),
+        []
+      );
+    };
 
-client.on("ready", () => {
-  console.log("ready");
+    getFiles("C:/github")
+      .then(flattenArray)
+      .then((files) => {
+        files.map((f) =>
+          fs.readFile(f, "utf8", (err, data) => {
+            amount = amount + data.split("\n").length;
+          })
+        );
+      });
 
-  const logMessage = () => {
-    console.log(
-      `${chalk.bold(
-        `[${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}] ${chalk.green(
-          "Successfully"
-        )} updated RPC!`
-      )}`
-    );
+    setTimeout(() => {
+      client.setActivity({
+        details: "Developing",
+        state: `${amount.toLocaleString(undefined, {
+          minimumFractionDigits: 0,
+        })} total lines of code`,
+        largeImageKey: `https://celeritycs.com/images/background.png`,
+        largeImageText: `www.celeritycs.com`,
+        smallImageKey: `https://celeritycs.com/images/white.png`,
+        smallImageText: `Celerity`,
+        instance: true,
+        buttons: [
+          {
+            label: "Website",
+            url: "https://celeritycs.com",
+          },
+        ],
+      });
+    }, 1000);
   };
 
-  updateActivity();
-  logMessage();
+  client.on("ready", () => {
+    console.log("ready");
 
-  setInterval(() => {
+    const logMessage = () => {
+      console.log(
+        `${chalk.bold(
+          `[${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}] ${chalk.green(
+            "Successfully"
+          )} updated RPC!`
+        )}`
+      );
+    };
+
     updateActivity();
     logMessage();
-  }, 60000);
-});
 
-client.login({ clientId });
+    setInterval(() => {
+      updateActivity();
+      logMessage();
+    }, 60000);
+  });
+
+  client.login({ clientId });
+};
+
+try {
+  run();
+} catch (err) {
+  run();
+}
